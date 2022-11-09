460f30cb1d0ad5d9606c29ffa3e6f3ea1a2150a2
bibadada <davideregnani@gmail.com>
2022-11-09 15:39:31 +0100

    Update() con ADO + formattazione colonna BirthDate nella view

--- UsersClassLibrary/Controllers/Users.cs
+++ UsersClassLibrary/Controllers/Users.cs
@@ -30,6 +30,30 @@ namespace UsersClassLibrary.Controllers
             
         }
 
+        private static User ReaderToBuildUser(SqlDataReader reader)
+        {
+            return new User
+            {
+                Id = (int)reader["Id"],
+                FirstName = (string)reader["FirstName"],
+                LastName = (string)reader["LastName"],
+                Age = (int)reader["Age"],
+                Gender = (string)reader["Gender"],
+                Email = (string)reader["Email"],
+                Username = (string)reader["Username"],
+                Password = (string)reader["Password"],
+                BirthDate = (DateTime)reader["BirthDate"],
+                Address = new FullAddress
+                {
+                    Address = (string)reader["Address"],
+                    City = (string)reader["City"],
+                    PostalCode = (string)reader["PostalCode"],
+                    State = (string)reader["State"]
+                }
+
+            };
+        }
+
         //public static List<User> FindAll(Predicate<User> condizione)
         public static List<User> FindAll(string nome, string sex)
         {
@@ -55,26 +79,7 @@ namespace UsersClassLibrary.Controllers
                     {
                         while (reader.Read())
                         {
-                            retVal.Add(new User
-                            {
-                                Id = (int)reader["Id"],
-                                FirstName = (string)reader["FirstName"],
-                                LastName = (string)reader["LastName"],
-                                Age = (int)reader["Age"],
-                                Gender = (string)reader["Gender"],
-                                Email = (string)reader["Email"],
-                                Username = (string)reader["Username"],
-                                Password = (string)reader["Password"],
-                                BirthDate = (DateTime)reader["BirthDate"],
-                                Address = new FullAddress
-                                { 
-                                    Address = (string)reader["Address"],
-                                    City = (string)reader["City"],
-                                    PostalCode = (string)reader["PostalCode"],
-                                    State = (string)reader["State"]
-                                }
-
-                            });
+                            retVal.Add(ReaderToBuildUser(reader));
 
                         }
                     }
@@ -112,28 +117,7 @@ namespace UsersClassLibrary.Controllers
                     {
                         if(reader.Read())
                         {
-                            User retVal = new User
-                            {
-                                Id = (int)reader["Id"],
-                                FirstName = (string)reader["FirstName"],
-                                LastName = (string)reader["LastName"],
-                                Age = (int)reader["Age"],
-                                Gender = (string)reader["Gender"],
-                                Email = (string)reader["Email"],
-                                Username = (string)reader["Username"],
-                                Password = (string)reader["Password"],
-                                BirthDate = (DateTime)reader["BirthDate"],
-                                Address = new FullAddress
-                                {
-                                    Address = (string)reader["Address"],
-                                    City = (string)reader["City"],
-                                    PostalCode = (string)reader["PostalCode"],
-                                    State = (string)reader["State"]
-                                }
-
-                            };
-                            return retVal;
-
+                            return ReaderToBuildUser(reader);
                         }
                         else
                         {
@@ -173,43 +157,19 @@ namespace UsersClassLibrary.Controllers
                     {
                         if (reader.Read())
                         {
-                            User retVal = new User
-                            {
-                                Id = (int)reader["Id"],
-                                FirstName = (string)reader["FirstName"],
-                                LastName = (string)reader["LastName"],
-                                Age = (int)reader["Age"],
-                                Gender = (string)reader["Gender"],
-                                Email = (string)reader["Email"],
-                                Username = (string)reader["Username"],
-                                Password = (string)reader["Password"],
-                                BirthDate = (DateTime)reader["BirthDate"],
-                                Address = new FullAddress
-                                {
-                                    Address = (string)reader["Address"],
-                                    City = (string)reader["City"],
-                                    PostalCode = (string)reader["PostalCode"],
-                                    State = (string)reader["State"]
-                                }
-
-                            };
-                            return retVal;
-
+                            return ReaderToBuildUser(reader);
                         }
                         else
                             return null;
                     }
                     //fine usingDataReader
-
                 }
                 catch (Exception)
                 {
 
                     throw;
                 }
-
             }
-
         }
 
         /*public static User Find(Predicate<User> condizione)
@@ -219,12 +179,106 @@ namespace UsersClassLibrary.Controllers
 
         public static void Add(User u)
         {
-            GetAll().Add(u);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = connection;
+                    command.CommandText = "INSERT INTO Users VALUES " +
+                    "(@Id, @FirstName, @LastName, @Age, @Gender, @Email, @Username, @Password, @BirthDate, @Address, @City, @PostalCode, @State)";
+                    command.Parameters.AddWithValue("@Id", u.Id);
+                    command.Parameters.AddWithValue("@FirstName", u.FirstName);
+                    command.Parameters.AddWithValue("@LastName", u.LastName);
+                    command.Parameters.AddWithValue("@Age", u.Age);
+                    command.Parameters.AddWithValue("@Gender", u.Gender);
+                    command.Parameters.AddWithValue("@Email", u.Email);
+                    command.Parameters.AddWithValue("@Username", u.Username);
+                    command.Parameters.AddWithValue("@Password", u.Password);
+                    command.Parameters.AddWithValue("@BirthDate", u.BirthDate);
+                    command.Parameters.AddWithValue("@Address", u.Address == null ? "" : u.Address.Address);
+                    command.Parameters.AddWithValue("@City", u.Address == null ? "" : u.Address.City);
+                    command.Parameters.AddWithValue("@PostalCode", u.Address == null ? "" : u.Address.PostalCode);
+                    command.Parameters.AddWithValue("@State", u.Address == null ? "" : u.Address.State);
+
+                    command.ExecuteNonQuery();
+
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
         }
 
         public static bool Update(int id, User u)
         {
             if (u.Id != id) return false;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = connection;
+                    command.CommandText = "UPDATE Users ";
+                    command.CommandText += "SET FirstName = @FirstName, ";
+                    command.Parameters.AddWithValue("@FirstName", u.FirstName);
+
+                    command.CommandText += "  LastName = @LastName, ";
+                    command.Parameters.AddWithValue("@LastName", u.LastName);
+
+                    command.CommandText += "  Age = @Age, ";
+                    command.Parameters.AddWithValue("@Age", u.Age);
+
+                    command.CommandText += "  Gender = @Gender, ";
+                    command.Parameters.AddWithValue("@Gender", u.Gender);
+
+                    command.CommandText += "  Email = @Email, ";
+                    command.Parameters.AddWithValue("@Email", u.Email);
+
+                    command.CommandText += "  Username = @Username, ";
+                    command.Parameters.AddWithValue("@Username", u.Username);
+
+                    command.CommandText += "  Password = @Password, ";
+                    command.Parameters.AddWithValue("@Password", u.Password);
+
+                    command.CommandText += "  BirthDate = @BirthDate, ";
+                    command.Parameters.AddWithValue("@BirthDate", u.BirthDate);
+
+                    command.CommandText += "  Address = @Address, ";
+                    command.Parameters.AddWithValue("@Address", u.Address == null ? "" : u.Address.Address);
+
+                    command.CommandText += "  City = @City, ";
+                    command.Parameters.AddWithValue("@City", u.Address == null ? "" : u.Address.City);
+
+                    command.CommandText += "  PostalCode = @PostalCode, ";
+                    command.Parameters.AddWithValue("@PostalCode", u.Address == null ? "" : u.Address.PostalCode);
+
+                    command.CommandText += "  State = @State ";
+                    command.Parameters.AddWithValue("@State", u.Address == null ? "" : u.Address.State);
+                    command.CommandText += "WHERE Id = @Id";
+                    command.Parameters.AddWithValue("@Id", u.Id);
+
+                    if (command.ExecuteNonQuery() != 1)
+                        return false;
+                    return true;
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+
+
+            /*
             User candidate = Find(id);
             if (candidate == null) return false;
 
@@ -238,6 +292,7 @@ namespace UsersClassLibrary.Controllers
             candidate.Gender = u.Gender;
 
             return true;
+            */
         }
         public static List<string> GetGenders()
         {
